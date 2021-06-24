e728b412a61bac203165a2adf3406b1e7b5f53fa
jose <nino.capati@gmail.com>
2021-06-24 21:48:42 +0800

    added default payment

--- payment/models.py
+++ payment/models.py
@@ -7,6 +7,7 @@ class PaymentMethod(models.Model):
     id = models.AutoField(primary_key=True)
     method = models.CharField(max_length=50, null=True, blank=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user2paymentmethod")
+    default = models.BooleanField(default=False)
     date_created = models.DateTimeField(auto_now_add=True) 
     date_modified = models.DateTimeField(auto_now=True) 
 
