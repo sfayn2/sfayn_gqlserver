ec840f94ca4bedf21d6761b4966d58179d3a14fc
jose <nino.capati@gmail.com>
2021-10-17 15:47:14 +0800

    enable cors frontend https://sfayn2.github.io/

--- sfayn_gqlserver/settings.py
+++ sfayn_gqlserver/settings.py
@@ -131,6 +131,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/') # media folder
@@ -162,6 +163,7 @@ GRAPHENE = {
 #CORS
 CORS_ORIGIN_WHITELIST = (
     'http://192.168.1.88:4200',
+    'https://sfayn2.github.io'
 )
 
 #for JWT Token
