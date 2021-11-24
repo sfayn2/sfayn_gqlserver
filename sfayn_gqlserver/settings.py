e9684965cbf355550d1b52f2ab25d1036658a0f7
Jose <nino.capati@gmail.com>
2021-11-24 21:32:48 +0800

    add back cb app

--- sfayn_gqlserver/settings.py
+++ sfayn_gqlserver/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'storages',
-    #'cb',
+    'cb',
     'graphene_django',
     'corsheaders',
     'product',
