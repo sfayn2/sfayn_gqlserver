4a419016012b7508808510ec299426625d98155b
Jose <nino.capati@gmail.com>
2023-04-24 21:50:23 +0800

    setup payment admin

--- payment/admin.py
+++ payment/admin.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
+from common import (
+        CommonAdmin,
+        get_list_display
+        )
 from .models import (
     PaymentMethod
 )
 
 # Register your models here.
-class PaymentMethodAdmin(admin.ModelAdmin):
+class PaymentMethodAdmin(CommonAdmin):
     search_fields = ("method",)
-    list_display = ("id", "method", )
     list_display_links = ("method",)
+    list_display = get_list_display(PaymentMethod, ("payment2order",))
 
 admin.site.register(PaymentMethod, PaymentMethodAdmin)
 
