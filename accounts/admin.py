da6480c163e5643df996d4815a89a3e5f2b540ff
Jose <nino.capati@gmail.com>
2023-04-24 10:17:56 +0800

    role module

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Address, GroupProfile
+from .models import Address, GroupProfile, Role
 from services import CommonAdmin
 
 # Register your models here.
@@ -11,10 +11,27 @@ class AddressAdmin(CommonAdmin):
 
 
 class GroupProfileAdmin(CommonAdmin):
+    filter_horizontal = ('role', )
     search_fields = ("name", )
-    list_display = ("id", "name", "role", "created_by", "date_created", "date_modified")
+    list_display = ("id", "name", "desc", "get_roles", "created_by", "date_created", "date_modified")
+    list_display_links = ("name",)
+
+    def get_roles(self, obj):
+        if obj.role:
+            return ", ".join([str(r) for r in obj.role.all()])
+        else:
+            return None
+
+
+    get_roles.short_description = "Roles"
+
+
+class RoleAdmin(CommonAdmin):
+    search_fields = ("name", )
+    list_display = ("id", "name", "created_by", "date_created", "date_modified")
     list_display_links = ("name",)
 
 
 admin.site.register(Address, AddressAdmin)
 admin.site.register(GroupProfile, GroupProfileAdmin)
+admin.site.register(Role, RoleAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -19,8 +19,26 @@ class Address(models.Model):
 
 
 class GroupProfile(Group):
-    role = models.CharField(max_length=50, help_text="use comma for multiple roles ex. VENDOR,STORE_OWNER")
+    role = models.ManyToManyField("accounts.Role", related_name="role2group")
+    desc = models.TextField(null=True, blank=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user2group")
     date_created = models.DateTimeField(auto_now_add=True) 
     date_modified = models.DateTimeField(auto_now=True) 
+
+    def get_roles(self):
+        return ", ".join(list(self.role.values_list('name', flat=True)))
+
+
+    def __str__(self):
+        return f"{self.name} - {self.desc} ({self.get_roles()})"
+
+
+class Role(models.Model):
+    name = models.CharField(max_length=20, help_text="ex. VENDOR")
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user2role")
+    date_created = models.DateTimeField(auto_now_add=True) 
+    date_modified = models.DateTimeField(auto_now=True) 
+
+    def __str__(self):
+        return self.name
 
