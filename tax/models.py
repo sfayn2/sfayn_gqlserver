e38394fde119780aa09ab687012fc4e0bd8788e7
Jose <nino.capati@gmail.com>
2023-04-29 16:24:05 +0800

    choices to boolean

--- tax/models.py
+++ tax/models.py
@@ -4,15 +4,11 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Tax(models.Model):
 
-    class Status(models.IntegerChoices):
-        ACTIVE = 0
-        IN_ACTIVE = 1
-
     name = models.CharField(max_length=20, help_text="GST, VAT, ?")
     country = models.CharField(max_length=50)
-    #region = models.CharField(max_length=50) by state or region??
+    region = models.CharField(max_length=50, blank=True, null=True) #by state or region??
     rate = models.FloatField(null=True, blank=True, help_text="N% tax rate per order?", verbose_name="Rate(%)")
-    status = models.IntegerField(null=True, choices=Status.choices) 
+    is_active = models.BooleanField(default=False)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user2tax")
     date_created = models.DateTimeField(auto_now_add=True) 
     date_modified = models.DateTimeField(auto_now=True) 
