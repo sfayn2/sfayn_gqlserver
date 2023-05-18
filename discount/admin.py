49433eae2f821f8ba82ebae636d85a9e33cf3699
Jose <nino.capati@gmail.com>
2023-05-18 23:16:11 +0800

    flattend discount rule

--- discount/admin.py
+++ discount/admin.py
@@ -1,27 +1,8 @@
 from django.contrib import admin
 from .models import (
     Discount,
-    DiscountTypePercentageOrFixAmount,
-    DiscountTypeBuyXGetX,
-    DiscountTypeVoucher
 )
 
-class DiscountTypePercentageOrFixAmountAdmin(admin.ModelAdmin):
-    search_fields = ("created_by",)
-    list_display = ("id", "name", "minimum_quantity", "by_percentage", "fix_amount", "start_date", "end_date", "created_by")
-    list_display_links = ("name", )
-
-
-class DiscountTypeBuyXGetXAdmin(admin.ModelAdmin):
-    search_fields = ("created_by",)
-    list_display = ("id", "name", "buy_quantity", "get_quantity", "start_date", "end_date", "created_by")
-    list_display_links = ("name", )
-
-
-class DiscountTypeVoucherAdmin(admin.ModelAdmin):
-    search_fields = ("created_by",)
-    list_display = ("id", "name", "voucher", "percent_offer", "fix_offer", "min_spend", "capped_at", "free_shipping", "usage_limit", "created_by")
-    list_display_links = ("name", )
 
 class DiscountAdmin(admin.ModelAdmin):
     filter_horizontal = ('vendor', 'tag', 'product_variant', 'category', 'shipping_method')
@@ -30,6 +11,3 @@ class DiscountAdmin(admin.ModelAdmin):
     list_display_links = ("name", )
 
 admin.site.register(Discount, DiscountAdmin)
-admin.site.register(DiscountTypePercentageOrFixAmount, DiscountTypePercentageOrFixAmountAdmin)
-admin.site.register(DiscountTypeBuyXGetX, DiscountTypeBuyXGetXAdmin)
-admin.site.register(DiscountTypeVoucher, DiscountTypeVoucherAdmin)
--- discount/models.py
+++ discount/models.py
@@ -7,17 +7,29 @@ from django.conf import settings
 
 # Create your models here.
 class Discount(models.Model):
+    minimum_quantity = models.IntegerField(null=True)
+    by_percentage = models.DecimalField(
+            decimal_places=settings.DEFAULT_DECIMAL_PLACES, 
+            max_digits=settings.DEFAULT_MAX_DIGITS,
+            null=True, 
+            blank=True, 
+            help_text="Discount by percentage", 
+        )
+    fix_amount = models.DecimalField(
+            decimal_places=settings.DEFAULT_DECIMAL_PLACES, 
+            max_digits=settings.DEFAULT_MAX_DIGITS,
+            null=True, 
+            blank=True, 
+            help_text="Discount by fix amount", 
+        )
 
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=25)
-    discount_type = models.ForeignKey(
-            ContentType, 
-            on_delete=models.CASCADE, 
-            help_text="Select a discount rules for shop, tags, or any product variants",
-            limit_choices_to={"app_label": "discount"}, #how to exclude self
+    discount_type = models.TextField(
+            help_text="ex. [ {'name': 'Discount by Fix or Percentage', 'by_percentage': 12 }, { 'name': 'Buy N get N', 'buy_qty': 5, 'get_qty': 1 } ]",
+            blank=True,
+            null=True
     )
-    object_id = models.PositiveIntegerField()
-    discount_object = GenericForeignKey("discount_type", "object_id")
     vendor = models.ManyToManyField('vendor.Vendor', blank=True, related_name="vendor2discount")
     tag = models.ManyToManyField('tag.Tag', blank=True, related_name="tag2discount")
     shipping_method = models.ManyToManyField('shipping.Method', blank=True, related_name="shipmethod2discount")
@@ -36,80 +48,39 @@ class Discount(models.Model):
         return self.name
 
 
-class DiscountTypeAbstract(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=25)
-    start_date = models.DateTimeField() 
-    end_date = models.DateTimeField() 
-    created_by = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE, 
-    )
-    date_created = models.DateTimeField(auto_now_add=True) 
-    date_modified = models.DateTimeField(auto_now=True) 
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        abstract = True
 
 
-
-class DiscountTypePercentageOrFixAmount(DiscountTypeAbstract):
-    minimum_quantity = models.IntegerField(null=True)
-    by_percentage = models.DecimalField(
-            decimal_places=settings.DEFAULT_DECIMAL_PLACES, 
-            max_digits=settings.DEFAULT_MAX_DIGITS,
-            null=True, 
-            blank=True, 
-            help_text="Discount by percentage", 
-        )
-    fix_amount = models.DecimalField(
-            decimal_places=settings.DEFAULT_DECIMAL_PLACES, 
-            max_digits=settings.DEFAULT_MAX_DIGITS,
-            null=True, 
-            blank=True, 
-            help_text="Discount by fix amount", 
-        )
-
-
-class DiscountTypeBuyXGetX(DiscountTypeAbstract):
-    buy_quantity = models.IntegerField(null=True)
-    get_quantity = models.IntegerField(null=True)
-
-
-class DiscountTypeVoucher(DiscountTypeAbstract):
-    voucher = models.CharField(max_length=15, help_text="Need to enter the voucher to use")
-    percent_offer = models.DecimalField(
-            decimal_places=settings.DEFAULT_DECIMAL_PLACES, 
-            max_digits=settings.DEFAULT_MAX_DIGITS,
-            null=True, 
-            blank=True, 
-            help_text="N% offer", 
-        )
-    fix_offer = models.DecimalField(
-            decimal_places=settings.DEFAULT_DECIMAL_PLACES, 
-            max_digits=settings.DEFAULT_MAX_DIGITS,
-            null=True, 
-            blank=True, 
-            help_text="fix amount offer", 
-        )
-    min_spend = models.DecimalField(
-            decimal_places=settings.DEFAULT_DECIMAL_PLACES, 
-            max_digits=settings.DEFAULT_MAX_DIGITS,
-            null=True, 
-            blank=True, 
-            help_text="min amount spend", 
-        )
-    capped_at = models.DecimalField(
-            decimal_places=settings.DEFAULT_DECIMAL_PLACES, 
-            max_digits=settings.DEFAULT_MAX_DIGITS,
-            null=True, 
-            blank=True, 
-            help_text="max discount price", 
-        )
-    free_shipping = models.BooleanField(default=False, help_text="select for free shipping")
-    usage_limit = models.IntegerField(default=1, help_text="limited to number of use")
-    img_upload = models.ImageField(upload_to=path_and_rename, null=True, blank=True, help_text="Voucher img")
+#class DiscountTypeVoucher(DiscountTypeAbstract):
+#    voucher = models.CharField(max_length=15, help_text="Need to enter the voucher to use")
+#    percent_offer = models.DecimalField(
+#            decimal_places=settings.DEFAULT_DECIMAL_PLACES, 
+#            max_digits=settings.DEFAULT_MAX_DIGITS,
+#            null=True, 
+#            blank=True, 
+#            help_text="N% offer", 
+#        )
+#    fix_offer = models.DecimalField(
+#            decimal_places=settings.DEFAULT_DECIMAL_PLACES, 
+#            max_digits=settings.DEFAULT_MAX_DIGITS,
+#            null=True, 
+#            blank=True, 
+#            help_text="fix amount offer", 
+#        )
+#    min_spend = models.DecimalField(
+#            decimal_places=settings.DEFAULT_DECIMAL_PLACES, 
+#            max_digits=settings.DEFAULT_MAX_DIGITS,
+#            null=True, 
+#            blank=True, 
+#            help_text="min amount spend", 
+#        )
+#    capped_at = models.DecimalField(
+#            decimal_places=settings.DEFAULT_DECIMAL_PLACES, 
+#            max_digits=settings.DEFAULT_MAX_DIGITS,
+#            null=True, 
+#            blank=True, 
+#            help_text="max discount price", 
+#        )
+#    free_shipping = models.BooleanField(default=False, help_text="select for free shipping")
+#    usage_limit = models.IntegerField(default=1, help_text="limited to number of use")
+#    img_upload = models.ImageField(upload_to=path_and_rename, null=True, blank=True, help_text="Voucher img")
 
