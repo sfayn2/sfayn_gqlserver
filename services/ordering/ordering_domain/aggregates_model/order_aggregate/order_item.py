7b92cdbf0873f7b112f32267a82edd8f3bdee51d
Jose <nino.capati@gmail.com>
2023-06-13 22:14:56 +0800

    uses unsafe hash for order item

--- services/ordering/ordering_domain/aggregates_model/order_aggregate/order_item.py
+++ services/ordering/ordering_domain/aggregates_model/order_aggregate/order_item.py
@@ -1,37 +1,27 @@
 from decimal import Decimal
+from dataclasses import dataclass
 from ....ordering_domain import abstract_domain_models
 
+@dataclass(unsafe_hash=True)
 class OrderItem(abstract_domain_models.Entity):
+    _order_quantity: int = 1
+    _product_id: str
+    _product_sn: str
+    _product_title: str
+    _product_variant_id: str
+    _product_variant_name: str
+    _product_price:  Decimal
+    _product_options: str
+    _product_img_url = str
+    _discounts_fee: Decimal
 
-    def __init__(
-        self
-        order_quantity: int = 1,
-        product_id: str,
-        product_sn: str,
-        product_title: str,
-        product_variant_id: str,
-        product_variant_name: str,
-        product_price:  Decimal,
-        product_options: str,
-        product_img_url = str,
-        discounts_fee: Decimal,
-    ):
-        if self.order_quantity <= 0:
+    def __post_init__(self):
+        if self._order_quantity <= 0:
             raise "Invalid quantity!"
 
-        if (self.product_price * self.order_quantity) < self.discounts_fee:
+        if (self._product_price * self._order_quantity) < self._discounts_fee:
             raise "Total amount order is lower than discounts"
 
-        self._add_order_quantity = order_quantity
-        self._product_id = product_id
-        self._product_sn = product_sn
-        self._product_title = product_title
-        self._product_variant_id = product_variant_id
-        self._product_variant_name = product_variant_name
-        self._product_price = product_price
-        self._product_options = product_options
-        self._product_img_url = product_img_url
-        self._discounts_fee = discounts_fee
 
     def change_discount(self, discount_fee: Decimal):
         self._discount_fee  = discount_fee
