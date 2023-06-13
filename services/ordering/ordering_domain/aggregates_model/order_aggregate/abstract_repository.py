6b8658789f846425abc55da66b94ba245ac16e58
Jose <nino.capati@gmail.com>
2023-06-13 22:13:53 +0800

    added order repository next_id method requirement

--- services/ordering/ordering_domain/aggregates_model/order_aggregate/abstract_repository.py
+++ services/ordering/ordering_domain/aggregates_model/order_aggregate/abstract_repository.py
@@ -1,14 +1,18 @@
 
 import abc
-from order_aggregate import order
+from .order_aggregate.order import Order
 
-class AbstractRepository(abc.ABC):
+class AbstractOrderRepository(abc.ABC):
     @abc.abstractmethod
-    def add(self, order: order.Ordering):
+    def add(self, order: Order):
         raise NotImplementedError
 
     @abc.abstractmethod
-    def get(self, order_id) -> order.Ordering:
+    def get(self, order_id) -> Order:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def next_id(self):
         raise NotImplementedError
 
 
