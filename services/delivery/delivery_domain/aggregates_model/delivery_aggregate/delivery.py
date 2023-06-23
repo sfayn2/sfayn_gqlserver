e3978f5146dc89ba92e087574440e3acebe2af2c
Jose <nino.capati@gmail.com>
2023-06-23 23:07:22 +0800

    package pricing partial code

--- services/delivery/delivery_domain/aggregates_model/delivery_aggregate/delivery.py
+++ services/delivery/delivery_domain/aggregates_model/delivery_aggregate/delivery.py
@@ -1,5 +1,6 @@
 from ....delivery_domain import abstract_domain_models
 from .delivery_package import DeliveryPackage
+from .package_pricing import PackagePricing
 
 class Delivery(abstract_domain_models.AggregateRoot):
 
@@ -9,3 +10,11 @@ class Delivery(abstract_domain_models.AggregateRoot):
                 ):
 
         self._delivery_package = set()
+        self._package_pricing = set()
+
+    def add_package_pricing(self, package_pricing: PackagePricing):
+        self._package_pricing.add(package_pricing)
+
+    def get_package_pricings(self):
+        return self._package_pricing
+
