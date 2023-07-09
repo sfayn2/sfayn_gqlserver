c0cdb5ee8e8f5f1d7e37d77e3fb0460e9e18a275
Jose <nino.capati@gmail.com>
2023-07-09 20:39:26 +0800

    aggregate order status

--- services/ordering/ordering_domain/aggregates_model/order_aggregate/ordering.py
+++ services/ordering/ordering_domain/aggregates_model/order_aggregate/ordering.py
@@ -3,6 +3,7 @@ from typing import Optional, List
 from ....ordering_domain import abstract_domain_models
 from ..buyer_aggregate.buyer import Buyer
 from .line_item import LineItem
+from .fulfillment_item import FulfillmentItem, FulfillmentStatus
 from .money import Money
 from .order_status import OrderStatus
 
@@ -14,12 +15,14 @@ class Ordering(abstract_domain_models.AggregateRoot):
                  tax_amount: Money,
                  buyer: Buyer,
                  line_items: List[LineItem],
-                 currency: str
+                 currency: str,
+                 payment_status: bool
                  ):
 
         self._entity_id = entity_id
         self._tax_amount = tax_amount
         self._currency = currency
+        self._payment_status = payment_status
 
         if buyer:
             self._buyer = buyer
@@ -58,19 +61,37 @@ class Ordering(abstract_domain_models.AggregateRoot):
     def set_fulfillment_items(self) -> None:
         if not self._line_items:
             raise "No item to fulfill!"
-        #TODO: create fulfilmment data
+        #TODO: create fulfilmment datagg
         self._fulfillment_items = self.add_line_items
 
-    def get_fulfillment_items(self):
+    def get_fulfillment_items(self) -> List[FulfillmentItem]:
         return self._fulfillment_items
 
-    def set_as_waiting_for_payment(self):
-        #if not self._order_status is None or hasattr(self, "_order_status"):
-        #    raise "Not allowed to set order status as waiting for payment!"
-        self._order_status = OrderStatus.WAITING_FOR_PAYMENT
+    def get_order_status(self):
+        if self._payment_status == True:
 
-    def set_as_paid(self):
-        self._order_status = OrderStatus.PAID
+            aggregate_status = [FulfillmentStatus.CANCELLED, 
+                                FulfillmentStatus.NOT_FULFILLED, 
+                                FulfillmentStatus.PROCESSING, 
+                                FulfillmentStatus.FULFILLED]
+
+            for status in aggregate_status:
+                for fulfill_item in self.get_fulfillment_items():
+                    if fulfill_item.get_fulfillment_status() == status:
+                        if status == FulfillmentStatus.CANCELLED:
+                            self._order_status = OrderStatus.CANCEL
+                        elif status == FulfillmentStatus.NOT_FULFILLED:
+                            self._order_status = OrderStatus.PAID
+                        elif status == FulfillmentStatus.PROCESSING:
+                            self._order_status = OrderStatus.PROCESSING
+                        elif status == FulfillmentStatus.FULFILLED:
+                            self._order_status = OrderStatus.COMPLETED
+                        return self._order_status
+
+            self._order_status = OrderStatus.PAID
+        else:
+            self._order_status = OrderStatus.WAITING_FOR_PAYMENT    
+
 
     def get_tax_amount(self):
         return self._tax_amount
