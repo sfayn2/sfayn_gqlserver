8fcf2d87d0db4240dfaa25b936e2c38c449b0e5d
josnin <nino.capati@gmail.com>
2024-12-14 18:57:07 +0800

    remove extra step of instantiating msg bus

--- ddd/product_catalog/app/message_bus.py
+++ ddd/product_catalog/app/message_bus.py
@@ -1,3 +1,4 @@
+from typing import Union
 from ddd.product_catalog.app import unit_of_work, handlers
 from ddd.product_catalog.domain import commands, events
 
@@ -9,27 +10,26 @@ EVENT_HANDLERS = {
     events.ProductActivated : [handlers.log_activated_product]
 }
 
-class MessageBus:
-    def handle(self, message, uow: unit_of_work.DjangoUnitOfWork):
-        """ dispatch message to appropriate handler(s) """
-        if isinstance(message, commands.Command):
-            handler = COMMAND_HANDLERS.get(type(message))
-            if not handler:
-                raise ValueError(f"No handler registered for command: {type(message)}")
-            event = handler(message, uow)
-            self._handle_event(event, uow)
-        elif isinstance(message, events.DomainEvent):
-            handlers = EVENT_HANDLERS.get(type(message), [])
-            for handler in handlers:
-                handler(message, uow)
-        else:
-            raise ValueError(f"Unknown message type {type(message)}")
-
-    def _handle_event(self, event, uow):
-        # handle the event locally by triggering event handlers
-        handlers = EVENT_HANDLERS.get(type(event), [])
+def handle(message: Union[commands.Command, events.DomainEvent], uow: unit_of_work.DjangoUnitOfWork):
+    """ dispatch message to appropriate handler(s) """
+    if isinstance(message, commands.Command):
+        handler = COMMAND_HANDLERS.get(type(message))
+        if not handler:
+            raise ValueError(f"No handler registered for command: {type(message)}")
+        event = handler(message, uow)
+        handle_event(event, uow)
+    elif isinstance(message, events.DomainEvent):
+        handlers = EVENT_HANDLERS.get(type(message), [])
         for handler in handlers:
-            handler(event, uow)
+            handler(message, uow)
+    else:
+        raise ValueError(f"Unknown message type {type(message)}")
 
-        # TODO?
-        # publish the event  using the event publisher (Redis, Kafka, RabbitMQ, etc.?)
+def handle_event(event, uow):
+    # handle the event locally by triggering event handlers
+    handlers = EVENT_HANDLERS.get(type(event), [])
+    for handler in handlers:
+        handler(event, uow)
+
+    # TODO?
+    # publish the event  using the event publisher (Redis, Kafka, RabbitMQ, etc.?)
--- ddd/product_catalog/entry_point.py
+++ ddd/product_catalog/entry_point.py
@@ -1,17 +1,17 @@
-from ddd.product_catalog.app import unit_of_work, message_bus
+from ddd.product_catalog.app import message_bus, unit_of_work
 from ddd.product_catalog.domain import commands
 
 def test_product_created():
     uow = unit_of_work.DjangoUnitOfWork
-    mb = message_bus.MessageBus()
     command = commands.CreateProductCommand(name="SmartPhone", price=100.00, category="Electronics")
 
-    mb.handle(command, uow)
+    message_bus.handle(command, uow)
 
 def test_activate_product():
     uow = unit_of_work.DjangoUnitOfWork()
-    msg = message_bus.MessageBus()
-    command = commands.ActivateProductCommand(product_id="e3bf4346-864a-4875-8ef3-ed3909f49e48")
+    cmd = commands.ActivateProductCommand(
+        product_id="e3bf4346-864a-4875-8ef3-ed3909f49e48"
+    )
 
-    msg.handle(command, uow)
+    message_bus.handle(cmd, uow)
 
