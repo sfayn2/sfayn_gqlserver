979436e351bfa1f59d07883f01e6c0eba8a285c1
josnin <nino.capati@gmail.com>
2024-12-16 22:02:59 +0800

    cleanup

--- _to_deprecate_shipping/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ShippingConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'shipping'
--- _to_deprecate_tax/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TaxConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'tax'
