58aad97bee1b5e294caf649e82a1c5b9ccb53724
josnin <nino.capati@gmail.com>
2025-01-15 22:02:13 +0800

    introduce customer management

---
+++ customer_management/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CustomerManagementConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'customer_management'
--- sfayn_gqlserver/settings.py
+++ sfayn_gqlserver/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
     'graphene_django',
     'product_catalog',
     'order_management',
-    'vendor_management'
+    'vendor_management',
+    'customer_management'
 ]
 
 MIDDLEWARE = [
