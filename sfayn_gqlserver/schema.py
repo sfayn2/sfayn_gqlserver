c3f12687a4a905986a841df1f00967134e6f2435
josnin <nino.capati@gmail.com>
2025-04-05 21:01:12 +0800

    interfaces to presentations to be more clear

--- sfayn_gqlserver/schema.py
+++ sfayn_gqlserver/schema.py
@@ -1,20 +1,20 @@
 import graphene
 import product_catalog.schema
-import ddd.order_management.interfaces.graphql.schema
+import ddd.order_management.presentations.graphql.schema
 
 class Query(
     product_catalog.schema.Query,
-    ddd.order_management.interfaces.graphql.schema.Query
+    ddd.order_management.presentations.graphql.schema.Query
 ):
     #This class will inherit from multiple queries
     #as we begin to add more apps to our project
     pass
 
 class Mutation(graphene.ObjectType):
-    checkout_items = ddd.order_management.interfaces.graphql.schema.CheckoutItemsMutation.Field()
-    place_order = ddd.order_management.interfaces.graphql.schema.PlaceOrderMutation.Field()
-    confirm_order = ddd.order_management.interfaces.graphql.schema.ConfirmOrderMutation.Field()
-    select_shipping_option = ddd.order_management.interfaces.graphql.schema.SelectShippingOptionMutation.Field()
+    checkout_items = ddd.order_management.presentations.graphql.schema.CheckoutItemsMutation.Field()
+    place_order = ddd.order_management.presentations.graphql.schema.PlaceOrderMutation.Field()
+    confirm_order = ddd.order_management.presentations.graphql.schema.ConfirmOrderMutation.Field()
+    select_shipping_option = ddd.order_management.presentations.graphql.schema.SelectShippingOptionMutation.Field()
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
 
