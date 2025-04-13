15fa710ef01350aab0ed97c467d4b95d46e0e7eb
josnin <nino.capati@gmail.com>
2025-04-13 11:02:06 +0800

    mv command / quer handler registration into bootstrap

--- ddd/order_management/application/message_bus.py
+++ ddd/order_management/application/message_bus.py
@@ -1,27 +1,11 @@
 
-from typing import Union
-from ddd.order_management.application import commands, handlers1, queries
-from ddd.order_management.application.handlers import (
-    place_order,
-    confirm_order,
-    select_shipping_option,
-    checkout_items,
-    get_shipping_options
-)
+from typing import Union, Dict, Callable
+from ddd.order_management.application import commands, queries
 from ddd.order_management.domain import repositories
 
+COMMAND_HANDLERS: Dict[commands.Command, Callable] = {}
 
-COMMAND_HANDLERS = {
-    commands.PlaceOrderCommand: place_order.handle_place_order,
-    commands.ConfirmOrderCommand: confirm_order.handle_confirm_order,
-    commands.SelectShippingOptionCommand: select_shipping_option.handle_select_shipping_option,
-    commands.CheckoutItemsCommand: checkout_items.handle_checkout_items,
-}
-
-QUERY_HANDLERS = {
-    queries.ShippingOptionsQuery: get_shipping_options.handle_shipping_options
-}
-
+QUERY_HANDLERS: Dict[queries.Query, Callable] = {}
 
 def handle(message: Union[commands.Command, queries.Query], uow: repositories.UnitOfWorkAbstract, dependencies: None):
     """ dispatch message to appropriate handler(s) """
--- ddd/order_management/infrastructure/bootstrap.py
+++ ddd/order_management/infrastructure/bootstrap.py
@@ -1,11 +1,21 @@
 
 from ddd.order_management.domain import events
-from ddd.order_management.application.handlers import event_handlers
 from ddd.order_management.infrastructure import event_bus
 from ddd.order_management.infrastructure.adapters import (
     email_service, 
     logging_service
 )
+from ddd.order_management.application.handlers import (
+    place_order,
+    confirm_order,
+    select_shipping_option,
+    checkout_items,
+    get_shipping_options,
+    event_handlers
+)
+
+from ddd.order_management.application import commands, message_bus, queries
+
 
 #Depending on the framework arch this might be inside manage.py , app.py, or main.py ?
 def register_event_handlers():
@@ -14,4 +24,17 @@ def register_event_handlers():
             lambda event, uow: event_handlers.handle_logged_order(event, uow, logging_service=logging_service.LoggingService()),
             lambda event, uow: event_handlers.handle_email_canceled_order(event, uow, email_service=email_service.EmailService())
             ]
+    })
+
+def register_command_handlers():
+    message_bus.COMMAND_HANDLERS.update({
+        commands.PlaceOrderCommand: place_order.handle_place_order,
+        commands.ConfirmOrderCommand: confirm_order.handle_confirm_order,
+        commands.SelectShippingOptionCommand: select_shipping_option.handle_select_shipping_option,
+        commands.CheckoutItemsCommand: checkout_items.handle_checkout_items,
+    })
+
+def register_query_handlers():
+    message_bus.QUERY_HANDLERS.update({
+        queries.ShippingOptionsQuery: get_shipping_options.handle_shipping_options
     })
