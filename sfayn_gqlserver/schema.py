a4207b4944fc13c0c3bcc77d94c1ce0008968650
josnin <nino.capati@gmail.com>
2025-05-10 19:43:34 +0800

    cleanup

--- sfayn_gqlserver/schema.py
+++ sfayn_gqlserver/schema.py
@@ -1,29 +1,20 @@
 import graphene
 import product_catalog.schema
-import ddd.order_management.presentation.graphql.mutations
-import ddd.order_management.presentation.graphql.mutations.checkout_items_mutation
-import ddd.order_management.presentation.graphql.mutations.confirm_order_mutation
-import ddd.order_management.presentation.graphql.mutations.place_order_mutation
-import ddd.order_management.presentation.graphql.mutations.select_shipping_option_mutation
-import ddd.order_management.presentation.graphql.queries
-import ddd.order_management.presentation.graphql.queries.shipping_options_query
-import ddd.order_management.presentation.graphql.schema
-from ddd.order_management.presentation.graphql import queries
+from ddd.order_management.presentation.graphql import queries, mutations
+
 
 
 class Query(
     product_catalog.schema.Query,
-    ddd.order_management.presentation.graphql.queries.ShippingOptionsQuery
+    queries.ShippingOptionsQuery
 ):
-    #This class will inherit from multiple queries
-    #as we begin to add more apps to our project
     pass
 
 class Mutation(graphene.ObjectType):
-    checkout_items = ddd.order_management.presentation.graphql.mutations.checkout_items_mutation.CheckoutItemsMutation.Field()
-    place_order = ddd.order_management.presentation.graphql.mutations.place_order_mutation.PlaceOrderMutation.Field()
-    confirm_order = ddd.order_management.presentation.graphql.mutations.confirm_order_mutation.ConfirmOrderMutation.Field()
-    select_shipping_option = ddd.order_management.presentation.graphql.mutations.select_shipping_option_mutation.SelectShippingOptionMutation.Field()
+    checkout_items = mutations.checkout_items_mutation.CheckoutItemsMutation.Field()
+    place_order = mutations.place_order_mutation.PlaceOrderMutation.Field()
+    confirm_order = mutations.confirm_order_mutation.ConfirmOrderMutation.Field()
+    select_shipping_option = mutations.select_shipping_option_mutation.SelectShippingOptionMutation.Field()
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
 
