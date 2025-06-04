880e2bf92caf1d468b1a64ae4dbfa36d12e93256
josnin <nino.capati@gmail.com>
2025-06-04 21:43:56 +0800

    addition vendor verification for checkout

--- ddd/order_management/application/handlers/checkout_items.py
+++ ddd/order_management/application/handlers/checkout_items.py
@@ -12,30 +12,38 @@ def handle_checkout_items(
         tax_service:  TaxStrategyServiceAbstract,
         product_vendor_validation_service: ProductVendorValidationServiceAbstract,
         order_service: OrderServiceAbstract) -> dtos.OrderResponseDTO:
-    with uow:
+    try:
+        with uow:
 
-        line_items = [mappers.LineItemMapper.to_domain(item) for item in command.line_items]
-        product_vendor_validation_service.ensure_line_items_vendor_is_valid(items=line_items)
+            customer_details = mappers.CustomerDetailsMapper.to_domain(command.customer_details)
+            shipping_address = mappers.AddressMapper.to_domain(command.shipping_address)
+            line_items = [mappers.LineItemMapper.to_domain(item) for item in command.line_items]
 
-        draft_order = order_service.create_draft_order(
-            customer_details=mappers.CustomerDetailsMapper.to_domain(command.customer_details),
-            shipping_address=mappers.AddressMapper.to_domain(command.shipping_address),
-            line_items=line_items
-        )
+            product_vendor_validation_service.ensure_line_items_vendor_is_valid(items=line_items)
 
-        tax_results = tax_service.calculate_all_taxes(draft_order)
-        draft_order.apply_tax_results(tax_results)
+            draft_order = order_service.create_draft_order(
+                customer_details=customer_details,
+                shipping_address=shipping_address,
+                line_items=line_items
+            )
 
+            tax_results = tax_service.calculate_all_taxes(draft_order)
+            draft_order.apply_tax_results(tax_results)
 
-        draft_order_dto = mappers.OrderResponseMapper.to_dto(
-            order=draft_order,
-            success=True,
-            message="Cart items successfully checkout."
-        )
+            draft_order_dto = mappers.OrderResponseMapper.to_dto(
+                order=draft_order,
+                success=True,
+                message="Cart items successfully checkout."
+            )
 
-        uow.order.save(draft_order)
-        uow.commit()
+            uow.order.save(draft_order)
+            uow.commit()
 
-        return draft_order_dto
+    except (exceptions.InvalidOrderOperation) as e:
+        draft_order_dto = shared.handle_invalid_order_operation(e)
+    except Exception as e:
+        draft_order_dto = shared.handle_unexpected_error(f"Unexpected error during checkout {e}")
+
+    return draft_order_dto
 
 
--- ddd/order_management/infrastructure/adapters/django_products_vendor_validation_service_adapter.py
+++ ddd/order_management/infrastructure/adapters/django_products_vendor_validation_service_adapter.py
@@ -5,13 +5,25 @@ from ddd.order_management.application import ports
 from ddd.order_management.infrastructure import django_mappers
 from ddd.order_management.domain import exceptions
 from vendor_management import models as django_vendor_models
+from product_catalog import models as django_product_models
 
 class DjangoProductsVendorValidationServiceAdapter(ports.ProductsVendorValidationServiceAbstract):
 
     def ensure_line_items_vendor_is_valid(self, items: List[models.LineItem]) -> None:
         for item in items:
-            res = django_vendor_models.Vendor.objects.filter(id=item.vendor.id, name=item.vendor.name, country=item.vendor.country)
-            if res.exists():
-                django_mappers.VendorDetailsMapper.to_domain(res)
-            else:
-                raise exceptions.VendorNotFoundException(f"Vendor {item.vendor.name} not found.")
+            try:
+                vendor = django_vendor_models.Vendor.objects.get(id=item.vendor.id)
+            except django_vendor_models.DoesNotExist:
+                raise exceptions.VendorNotFoundException(f"Vendor {item.vendor.id} not found.")
+
+            try:
+                product = django_product_models.VendorItem.objects.get(sku=item.product_sku, product__vendor_id=item.vendor.id)
+            except django_product_models.DoesNotExist:
+                raise exceptions.VendorNotFoundException(f"Product SKU {item.product_sku} is not owned by Vendor ID {item.vendor.id}.")
+
+            # Fill in the missing vendor details
+            item.vendor = value_objects.VendorDetails(
+                id=vendor.id,
+                name=vendor.name,
+                country=vendor.country
+            )
