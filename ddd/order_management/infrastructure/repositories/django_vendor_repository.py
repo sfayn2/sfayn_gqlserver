40f6cf129ff332e81452c06a8b707c126a8afc12
josnin <nino.capati@gmail.com>
2025-06-12 21:58:57 +0800

    cleanup

--- ddd/order_management/infrastructure/repositories/django_vendor_repository.py
+++ ddd/order_management/infrastructure/repositories/django_vendor_repository.py
@@ -10,7 +10,7 @@ class DjangoVendorRepositoryImpl(repositories.VendorAbstract):
     def get_offers(self, vendor_id: uuid.UUID) -> List[value_objects.OfferStrategy]:
         offers = django_snapshots.VendorOfferSnapshot.objects.filter(vendor__id=vendor_id, is_active=True).prefetch_related("coupon").values()
         offer_list = [
-            { **offer, "coupons": list(django_snapshots.VendorCouponSnapshot.objects.filter(offer__id=offer.get("id")).values()) }
+            { **offer, "coupons": list(django_snapshots.VendorCouponSnapshot.objects.filter(offer_id=offer.get("offer_id")).values()) }
             for offer in offers
         ]
 
@@ -26,7 +26,7 @@ class DjangoVendorRepositoryImpl(repositories.VendorAbstract):
 
 
     def get_shipping_options(self, vendor_id: uuid.UUID) -> List[value_objects.ShippingOptionStrategy]:
-        shipping_options = django_snapshots.VendorShippingOptionSnapshot.objects.filter(vendor__id=vendor_id, is_active=True)
+        shipping_options = django_snapshots.VendorShippingOptionSnapshot.objects.filter(vendor_id=vendor_id, is_active=True)
         final_opts = []
         for option in shipping_options.values():
             option.pop("id")
