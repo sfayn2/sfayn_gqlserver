97f6e15cfd4ff73761122f1b78634e2610e6b606
josnin <nino.capati@gmail.com>
2025-09-11 21:04:36 +0800

    optimiz test rm line items

---
+++ ddd/order_management/tests/integration/handlers/test_remove_line_items.py
@@ -0,0 +1,68 @@
+import pytest, os, json, time
+from ddd.order_management.application import (
+    commands,
+    handlers,
+    dtos
+)
+from ddd.order_management.domain import (
+    services as domain_services,
+)
+from ddd.order_management.infrastructure import (
+    repositories as infra_repo,
+    access_control1,
+    validations
+)
+
+@pytest.fixture
+def fake_product_skus(test_constants):
+    return {
+        "valid": [dtos.ProductSkusDTO(vendor_id=test_constants["vendor1"], product_sku="sku_remove1", order_quantity=22)],
+        "not_allowed_remove_last_item": [
+            dtos.ProductSkusDTO(vendor_id=test_constants["vendor1"], product_sku="sku_remove1", order_quantity=22),
+            dtos.ProductSkusDTO(vendor_id=test_constants["vendor1"], product_sku="sku_remove2", order_quantity=22)
+        ]
+    }
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    "order_id, product_key, expected_success, expected_message",
+    [
+        ("ORD-REMOVEITEMS-1", "valid", True, "Order ORD-REMOVEITEMS-1 successfully remove line items."),
+        ("ORD-REMOVEITEMS-1", "not_allowed_remove_last_item", False, "Cannot remove the last item. Please cancel the order instead."),
+    ]
+)
+def test_remove_line_items(
+    fake_customer_details,
+    fake_address,
+    fake_product_skus,
+    fake_jwt_handler, 
+    fake_access_control,
+    domain_clock,
+    order_id,
+    product_key,
+    expected_success,
+    expected_message
+):
+
+    access_control = access_control1.AccessControl1(
+        jwt_handler=fake_jwt_handler()
+    )
+
+    command = commands.RemoveLineItemsCommand(
+        order_id=order_id,
+        product_skus=fake_product_skus[product_key]
+    )
+
+    user_ctx = fake_access_control().get_user_context(token="fake_jwt_token")
+
+    response = handlers.handle_remove_line_items(
+        command=command,
+        uow=infra_repo.DjangoOrderUnitOfWork(),
+        vendor_repo=infra_repo.DjangoVendorRepositoryImpl(),
+        access_control=fake_access_control(),
+        user_ctx=user_ctx
+    )
+
+
+    assert response.success is expected_success
+    assert response.message == expected_message
